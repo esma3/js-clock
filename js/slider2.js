8275935062f3f4b42447dd477e57626e5859159a
esma3 <semababayeva9898@gmail.com>
2022-09-28 02:11:59 +0400

    clockjs

---
+++ js/slider2.js
@@ -0,0 +1,15 @@
+// document.getElementById("demo").innerHTML="sen ne isle mesgulsan"
+// var ad ="rza"
+// var age =67;
+// var qaliq =1.50
+// var car=['']
+// var person ={}
+// var book =undefined
+// var water =null
+// var mercedes
+// console.log( typeof book)
+// document.getElementsByClassName('fema')[0].innerHTML = age+"  my age"
+
+// ad ="rza"
+// soyad ="talibov"
+// console.log(ad +"~ ~"+ "soyad")
